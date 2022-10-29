43c8e9b28e0fc9f2670280cfdf8b625ddd8333ad
MatteoPossamai <mpossamaim@gmail.com>
2022-10-29 18:00:28 +0200

    minor changes to scripts for the sake of testing

--- scraper/main.py
+++ scraper/main.py
@@ -2,6 +2,7 @@ import scraper
 
 if __name__ == '__main__':
     
-    scraper4 = scraper.WikiScraper('https://fr.wikipedia.org/wiki/Paris_Saint-Germain_Football_Club')
+    scraper4 = scraper.WikiScraper('https://it.wikipedia.org/wiki/Juventus_Football_Club')
     scraper4.scrape()
-    roaster = scraper4.get_team_roster()
+    info = scraper4.get_team_informations()
+    print(info)
