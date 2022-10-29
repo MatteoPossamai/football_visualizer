c2ec7694740d64d95d27e5cc94b54f9b10ab8032
MatteoPossamai <mpossamaim@gmail.com>
2022-10-29 22:05:45 +0200

    started plannign frontend itself

--- frontend/football_dashboard/src/App.tsx
+++ frontend/football_dashboard/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+// Components
+import Base from './components/base';
+
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Base />
     </div>
   );
 }
---
+++ frontend/football_dashboard/src/components/base.tsx
@@ -0,0 +1,13 @@
+import Header  from "./header";
+
+function Base(){
+    return (
+        <div>
+            <Header />
+            <div> CONTENT </div>
+            <footer> FOOTER </footer>
+        </div>
+    )
+}
+
+export default Base;
